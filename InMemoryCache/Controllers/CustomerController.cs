18c0341db1ccdba81cfbf3c7002069f053121a48
Andr3wDev <29abrown@gmail.com>
2022-03-04 14:17:25 +1300

    Example of in-memory cache technique

---
+++ InMemoryCache/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using InMemoryCache.Data;
+using InMemoryCache.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InMemoryCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMemoryCache memoryCache;
+        private readonly ApplicationDbContext context;
+        
+        public CustomerController(IMemoryCache memoryCache, ApplicationDbContext context)
+        {
+            this.memoryCache = memoryCache;
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var cacheKey = "customerList";
+
+            if (!memoryCache.TryGetValue(cacheKey, out List<Customer> customerList))
+            {
+                customerList = await context.Customers.ToListAsync();
+
+                var cacheExpiryOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                    Priority = CacheItemPriority.High,
+                    SlidingExpiration = TimeSpan.FromMinutes(2)
+                };
+                memoryCache.Set(cacheKey, customerList, cacheExpiryOptions);
+            }
+
+            return Ok(customerList);
+        }
+    }
+}
---
+++ InMemoryCache/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InMemoryCache.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InMemoryCache.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
